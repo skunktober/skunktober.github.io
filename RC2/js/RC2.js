1007e256a1304de4c768abf93f0c8abee04f17d1
Avery <126778577+snailvinyl@users.noreply.github.com>
2023-10-21 01:21:33 +0100

    Delete RC2 directory

--- RC2/js/RC2.js
+++
@@ -1,9 +0,0 @@
-// JavaScript function to handle button click animation
-function handleButtonClick(button) {
-    button.classList.add('btn-click-animation');
-
-    // Remove the animation class after the animation is complete
-    setTimeout(function () {
-        button.classList.remove('btn-click-animation');
-    }, 200); // 200ms is the duration of the animation defined in CSS
-}
