6c78dc5f92cfaab98f0965119c2c214da92d046d
Jack <126778577+snailvinyl@users.noreply.github.com>
2023-11-07 21:43:31 +0000

    Delete RC2 directory

--- RC2/js/Animation.js
+++
@@ -1,7 +0,0 @@
-function handleButtonClick(button) {
-    button.classList.add('btn-click-animation');
-
-    setTimeout(function () {
-        button.classList.remove('btn-click-animation');
-    }, 200);
-}
