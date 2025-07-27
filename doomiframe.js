4476dc963e4332ba77aeba8f4f6eed27815beeed
Jack <126778577+skunktober@users.noreply.github.com>
2025-07-27 22:36:19 +0100

    general fuckery

---
+++ doomiframe.js
@@ -0,0 +1,6 @@
+var iframe = document.createElement('iframe');
+iframe.src = "https://diekmann.github.io/wasm-fizzbuzz/doom/";
+iframe.width = "650";
+iframe.height = "600";
+iframe.title = "doom";
+document.body.appendChild(iframe);
